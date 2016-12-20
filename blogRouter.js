7bf8cd57161642577c57fc55a73a30adaeda7312
tindy360 <tindy360@gmail.com>
2016-12-20 11:16:00 -0500

    put and post added

--- blogRouter.js
+++ blogRouter.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 const express = require('express');
-const router = express.Router();
+const router = express.Router('');
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', jsonParser, (req, res) =>{
-  const requireFields = ['name', 'checked'];
+  const requireFields = ['title', 'content', 'author', 'Date'];
     for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -21,6 +21,37 @@ router.post('/', jsonParser, (req, res) =>{
       return res.status(400).send(message);
     }
   }
-  const item = BlogPosts.create(req.body.name, req.body.checked);
+  const item = BlogPosts.create(
+    req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
+});
+
+router.put('/:id', jsonParser,(req,res) =>{
+  const requireFields = [
+  'id', 'title', 'content', 'author', 'publishDate'];
+
+  for (let i=0; i<requiredFields.length; i++) {
+   const field = requiredFields[i];
+   if (!(field in req.body)) {
+     const message = `Missing \`${field}\` in request body`;
+     console.error(message);
+     return res.status(400).send(message);
+   }
+ }
+ if (req.params.id !== req.body.id) {
+   const message = (
+     `Request path id (${req.params.id}) and request body id `
+     `(${req.body.id}) must match`);
+   console.error(message);
+   return res.status(400).send(message);
+ }
+ console.log(`Updating blog post with id \`${req.params.id}\``);
+ const updatedItem = BlogPosts.update({
+   id: req.params.id,
+   title: req.body.title,
+   content: req.body.content,
+   author: req.body.author,
+   publishDate: req.body.publishDate
+ });
+ res.status(204).json(updatedItem);
 });
