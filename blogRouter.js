c5e440c906cbbd8cbcd3e7054c31b7bf6fb7accb
tindy360 <tindy360@gmail.com>
2016-12-20 08:24:01 -0500

    WIP w get route

--- blogRouter.js
+++ blogRouter.js
@@ -3,3 +3,24 @@ const express = require('express');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./module');
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get);
+});
+
+router.post('/', jsonParser, (req, res) =>{
+  const requireFields = ['name', 'checked'];
+    for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(req.body.name, req.body.checked);
+  res.status(201).json(item);
+});
--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const morgan = require ('morgan');
 //const {BlogPosts} = require ('./module');
 const app = express();
 
+const blogRouter = require('./blogRouter.js');
+
 app.use(morgan('common'));
 app.use(express.static('public'));
+
+app.use('./blogRouter', blogRouter);
+
 
 app.listen(process.env.PORT  || 8080, () =>{
   console.log('App running on${porocess.env.PORT || 8080}');
