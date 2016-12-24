9a121a1552d55e2eb747ef521334358c2f3bedf0
tindy360 <tindy360@gmail.com>
2016-12-24 10:46:08 -0500

    inital no testy testy

--- blogRouter.js
+++ blogRouter.js
@@ -1,62 +1,78 @@
 const express = require('express');
-const {BlogPosts} = require('./module');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const {BlogPosts} = require('./models');
 
+
+
+// seed some posts so initial GET requests will return something
+BlogPosts.create(
+  '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
+BlogPosts.create(
+  'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
+
+
+// adding get route returning json
 router.get('/', (req, res) => {
-  res.json({BlogPosts});
-  console.error(message);
-  return res.status(201).json(message)
+  res.json(BlogPosts.get());
 });
 
 
-router.post('/', jsonParser, (req, res) =>{
-  const requireFields = ['title', 'content', 'author', 'Date'];
-    for (let i=0; i<requiredFields.length; i++) {
+
+router.post('/', jsonParser, (req, res) => {
+//roupte for posting new posts with required fileds and error push for missing
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
+      const message = `Missing \`${field}\` in request body`
       console.error(message);
-      return res.status(400).json(message);
+      return res.status(400).send(message);
     }
   }
   const item = BlogPosts.create(
-    req.body.title, req.body.content, req.body.author);
+  	req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
-router.put('/:id', jsonParser,(req,res) =>{
-  const requireFields = [
-  'id', 'title', 'content', 'author', 'publishDate'];
 
+//ad put route for blog post updates/ edits with requested feilsd
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+  	'id', 'title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
-   const field = requiredFields[i];
-   if (!(field in req.body)) {
-     const message = `Missing \`${field}\` in request body`
-     console.error(message);
-     return res.status(400).send(message);
-   }
- }
- if (req.params.id !== req.body.id) {
-   const message = (
-     `Request path id (${req.params.id}) and request body id `
-     `(${req.body.id}) must match`);
-   console.error(message);
-   return res.status(400).send(message);
- }
- console.log(`Updating blog post with id \`${req.params.id}\``);
- const updatedItem = BlogPosts.update({
-   id: req.params.id,
-   title: req.body.title,
-   content: req.body.content,
-   author: req.body.author,
-   publishDate: req.body.publishDate
- });
- res.status(204).json(updatedItem);
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
 });
+
+
+// add endpoint for DELETE requests. These requests should
+// have an id as a URL path variable and call
+// `BlogPosts.delete()`
 router.delete('/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
   console.log(`Deleted blog post with id \`${req.params.ID}\``);
